151638bf4666c89432537468cda845d7ffbed992
uraiporn.k <31133546+uraiporn2539@users.noreply.github.com>
2017-12-13 23:31:04 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.Write("Hello");
+
+        }
+    }
+}
