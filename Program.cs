c6887fc05f2b3c46dfe0c9f623273acded1a2c98
uraiporn.k <31133546+uraiporn2539@users.noreply.github.com>
2017-12-14 00:27:15 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+           char ch;
+        	Console.Write("Press a key followed by ENTER:");
+        	ch = (char)Console.Read();  // get a char
+        	Console.WriteLine("Your key is: "+ch);
+
 
         }
     }
