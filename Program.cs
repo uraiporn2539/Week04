4159f39a8dc11f154b326cd639d01011a9d93534
uraiporn.k <31133546+uraiporn2539@users.noreply.github.com>
2017-12-13 23:46:41 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,7 +7,16 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("00000000011111111112");
+            Console.WriteLine("12345678901234567890");
+            Console.WriteLine("{0, 0}", 1);
+            Console.WriteLine("{0, 1}", 1);
+            Console.WriteLine("{0, 2}", 1);
+            Console.WriteLine("{0, 3}", 1);
+            Console.WriteLine("{0, 5}", 1);
+            Console.WriteLine("{0, 10}", 1);
+            Console.WriteLine("{0, 15}", 1);
+            Console.WriteLine("{0, 20}", 1);
 
         }
     }
