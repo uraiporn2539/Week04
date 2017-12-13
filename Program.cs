618f1cac5f777fb4bbd98a9f43bf006d2d3acc85
uraiporn.k <31133546+uraiporn2539@users.noreply.github.com>
2017-12-14 00:27:56 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+           const double i = 123.456789d;
+        	Console.WriteLine("{0:F1}", i);
+        	Console.WriteLine("{0:F2}", i);
+        	Console.WriteLine("{0:F3}", i);
+        	Console.WriteLine("{0:F4}", i);
+        	Console.WriteLine("{0:F5}", i);
 
         }
     }
