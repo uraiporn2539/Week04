2858ff6018f73e04d8ae5ab09869a57696eb996c
uraiporn.k <31133546+uraiporn2539@users.noreply.github.com>
2017-12-14 01:39:24 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
-﻿using System;
-
-
+using System;
 namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello");
-
+    	  {
+        	try
+        	{
+            	    Console.Write("Please enter value 1 :");
+            	    int val1 = Convert.ToInt32(Console.ReadLine());
+            	    Console.Write("Please enter value 2 :");
+            	    int val2 = Convert.ToInt32(Console.ReadLine());
+            	    Console.WriteLine("Answer = " + (val1 + val2));
+        	}
+        	catch (Exception e)
+        	{
+            	    Console.WriteLine("Error : " + e.ToString());
+        	}
         }
     }
 }
+
